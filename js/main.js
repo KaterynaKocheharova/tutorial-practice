c4c9d1279b4f3db41ce1005107fb25846ad2ad02
KaterynaKocheharova <kot41901@gmail.com>
2024-02-18 21:57:52 +0200

    scroll header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // =========================================== HOME SWIPER ===================================================
 
 const swiper = new Swiper('.home-swiper', {
+  // ??? what is it
   spaceBetween: 30,
   loop: true,
   pagination: {
@@ -8,3 +9,13 @@ const swiper = new Swiper('.home-swiper', {
     clickable: true,
   },
 });
+
+// ===================================== CHANGE BACKGROUND HEADER ===========================================
+
+function scrollHeader() {
+  const header = document.querySelector('header');
+  if (this.scrollY >= 50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
